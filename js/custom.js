cc0390f074ea2e86fe699a8b8baceca8b31183c0
kim myeong jin <gimm07584@gmail.com>
2024-12-17 16:45:33 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    const MAINVISUALSLIDE = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed: 2000,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: ["-20%", 0, -1],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
+        },
+
+        navigation: {
+            nextEl: '.swiper .arrows .next',
+            prevEl: '.swiper .arrows .prev',
+        },
+
+    })
+
+
+
+
+
+})
